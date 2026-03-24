9c073a25ce916440db9c07e637814e4f10cee673
Adscn <122090307@link.cuhk.edu.cn>
2026-03-24 23:26:39 +0800

    Set Default to be True

--- train_gpt_sota.py
+++ train_gpt_sota.py
@@ -96,7 +96,7 @@ class Hyperparameters:
     rope_dims = int(os.environ.get("ROPE_DIMS", 16))
     ln_scale = bool(int(os.environ.get("LN_SCALE", "1")))
     dtg_enabled = bool(int(os.environ.get("DTG_ENABLED", "0")))
-    rescale_resid = bool(int(os.environ.get("RESCALE_RESID", "0")))
+    rescale_resid = bool(int(os.environ.get("RESCALE_RESID", "1")))
     late_qat_threshold = float(os.environ.get("LATE_QAT_THRESHOLD", 0.15))
     ve_enabled = bool(int(os.environ.get("VE_ENABLED", "1")))
     ve_dim = int(os.environ.get("VE_DIM", 128))
